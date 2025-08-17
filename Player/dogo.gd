50a2aaab11fc050cb01957d130007438afac893b
JhannerAlmeida <129618808+JhannerAlmeida@users.noreply.github.com>
2025-08-17 19:08:23 -0300

    gravity and jump changed

--- Player/dogo.gd
+++ Player/dogo.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 250.0
+const JUMP_VELOCITY = -250.0
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("resart"):
+		get_tree().reload_current_scene()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,7 +14,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
