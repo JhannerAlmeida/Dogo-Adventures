0f3878b6de071fcf498bb4eee2362273eff7d9f6
JhannerAlmeida <129618808+JhannerAlmeida@users.noreply.github.com>
2025-08-19 23:11:00 -0300

    56

--- Player/dogo.gd
+++ Player/dogo.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
+const SPEED = 260.0
 const JUMP_VELOCITY = -300.0
 
 func _input(event: InputEvent) -> void:
--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-# warning-ignore:unused_argument
 func _on_body_entered(body):
 	print("+1 petisco")
 	queue_free()
