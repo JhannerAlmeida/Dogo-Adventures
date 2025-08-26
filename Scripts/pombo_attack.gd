5d4aaa5cb628e4d8ba7153a125923b9dafb2c029
JhannerAlmeida <129618808+JhannerAlmeida@users.noreply.github.com>
2025-08-26 10:22:11 -0300

    Pequenas mudanças

--- Scripts/pombo_attack.gd
+++ Scripts/pombo_attack.gd
@@ -11,9 +11,9 @@ var direction = 1
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
-		sprite.flip_h = false
+		sprite.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
-		sprite.flip_h = true
+		sprite.flip_h = false
 	
 	position.x += direction * SPEED * delta
