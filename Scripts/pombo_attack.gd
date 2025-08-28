9cb29913e50401ca230f8e4997080011d22efb05
BlueGamer4747 <danpacman2008@gmail.com>
2025-08-28 18:49:25 -0300

    Inimigo Rato

--- Scripts/pombo_attack.gd
+++ Scripts/pombo_attack.gd
@@ -2,18 +2,22 @@ extends CharacterBody2D
 
 const SPEED = 60
 
-var direction = 1
+var direction := 1
 
-@onready  var ray_cast_left = $RayCastLeft
-@onready var ray_cast_right = $RayCastRight
-@onready var sprite = $Sprite2D
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	sprite.play("Walk")
 	if ray_cast_right.is_colliding():
 		direction = -1
-		sprite.flip_h = true
-	if ray_cast_left.is_colliding():
-		direction = 1
 		sprite.flip_h = false
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		sprite.flip_h = true
+
+	velocity.x = direction * SPEED
+	move_and_slide()
+
 	
-	position.x += direction * SPEED * delta
